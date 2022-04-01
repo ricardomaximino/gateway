4d72644870591e65b3dbcfed205cada51bd513c8
Ricardo Maximino <ricardomaximino@hotmail.com>
2022-04-01 20:24:53 +0200

    Fist commit

---
+++ src/main/java/com/brasatech/gateway/route/RouteConfiguration.java
@@ -0,0 +1,33 @@
+package com.brasatech.gateway.route;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+public class RouteConfiguration {
+
+    @Bean
+    public UriConfiguration uriConfiguration(){
+        return new UriConfiguration();
+    }
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration){
+        String httpUri = uriConfiguration.getHttpbin();
+        return  builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+}
---
+++ src/main/java/com/brasatech/gateway/route/UriConfiguration.java
@@ -0,0 +1,15 @@
+package com.brasatech.gateway.route;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties
+public class UriConfiguration {
+
+    @Getter
+    @Setter
+    private String httpbin = "http://httpbin.org:80";
+
+}
